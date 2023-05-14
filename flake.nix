cb2a1332b690d39d49cb73fe46f5d402086f3bc6
StepBroBD <Hi@StepBroBD.com>
2023-05-14 16:44:40 -0600

    fix build

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
                   if nixpkgs.lib.hasInfix "linux" system then
                     builtins.fetchurl (osu)
                   else
-                    nixpkgs.pkgs.fetchzip (osu);
+                    pkgs.fetchzip (osu);
               });
             })
           ];
